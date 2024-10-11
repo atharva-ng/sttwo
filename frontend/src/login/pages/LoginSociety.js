1c31976d0cf151f663d07607a6a114f4b0119bf1
Om Priyansh <ompriyansh24@gmail.com>
2024-10-12 04:13:57 +0530

    Completed Society sign up Page

--- frontend/src/login/pages/LoginSociety.js
+++ frontend/src/login/pages/LoginSociety.js
@@ -6,8 +6,8 @@ import Form from "../Form";
 const LoginSociety = () => {
   const { userType, login, logout } = useContext(AuthContext);
   return (
-    <div className="form-container">
-      <h1>Society Login Form:</h1>
+    <div className="form-container-signup">
+      <h1>Society Login:</h1>
       <Form url="http://3.109.108.99:5007/api/auth/login/society" login={login} logout={logout} userType={userType} />
     </div>
   );
